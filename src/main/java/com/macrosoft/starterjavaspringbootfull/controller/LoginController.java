bef29f5c765dcbc7381226c078933d06674723d3
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2023-10-17 19:48:21 +0000

    Update

--- src/main/java/com/macrosoft/starterjavaspringbootfull/controller/LoginController.java
+++ src/main/java/com/macrosoft/starterjavaspringbootfull/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.macrosoft.starterjavaspringbootfull.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,17 +13,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @GetMapping("/loginecole")
     public String login() {
         return "loginecole";
     }
-    
+
     @PostMapping("/loginecole")
     public String performLogin(@RequestParam String username, @RequestParam String password) {
-        // Logique d'authentification ici (par exemple, utiliser Spring Security)
-        // Rediriger vers la page d'accueil si l'authentification réussit, sinon vers la page de connexion avec un message d'erreur
-        // return "redirect:/home"; // Redirige vers la page d'accueil après l'authentification réussie
-        // return "redirect:/loginecole?error=true"; // Redirige vers la page de connexion avec un message d'erreur
-        return "redirect:/loginecole?logout=true"; // Redirige vers la page de connexion avec un message de déconnexion
+        try {
+            Authentication authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            // Votre logique d'authentification ici (par exemple, vérifier le rôle de l'utilisateur)
+            // Rediriger vers la page d'accueil si l'authentification réussit
+            return "redirect:/indexecole";
+        } catch (Exception e) {
+            // Gérer les erreurs d'authentification (par exemple, mot de passe incorrect)
+            // Rediriger vers la page de connexion avec un message d'erreur
+            return "redirect:/loginecole?error=true";
+        }
     }
 }
